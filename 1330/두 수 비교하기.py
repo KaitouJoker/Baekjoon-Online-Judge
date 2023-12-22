6f4702797f439520ee026438d9e7c5d7bfd21202
Momil_Gugsu <aksdhksrud7@gmail.com>
2023-12-23 03:47:29 +0900

    code update

--- 1330/두 수 비교하기.py
+++ 1330/두 수 비교하기.py
@@ -1,9 +1,6 @@
-t = input().split()
-t = list(map(int, t))
+to:list[str] = input().split()
+t :list[int] = list(map(int, to))
 
-if t[0] == t[1]:
-    print("==")
-elif t[0] < t[1]:
-    print("<")
-else:
-    print(">")
+if   t[0] == t[1]: print("==")
+elif t[0] <  t[1]: print("<")
+else             : print(">")
