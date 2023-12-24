81cedd688401566c77baff8f5969e7542bb57274
Momil_Gugsu <aksdhksrud7@gmail.com>
2023-12-25 01:34:27 +0900

    code update

--- 5355/화성 수학.py
+++ 5355/화성 수학.py
@@ -1,8 +1,9 @@
-cmd = {'@' : lambda x : x * 3,
-       '%' : lambda x : x + 5,
-       '#' : lambda x : x - 7}
+from typing import Final
+cmd:Final = {'@' : lambda x : x * 3,
+             '%' : lambda x : x + 5,
+             '#' : lambda x : x - 7}
 for _ in range(int(input())):
     n, *cm = input().split()
-    n = float(n)
+    n:float = float(n)
     for c in cm: n = cmd[c](n)
     print(f'{n:.2f}')
